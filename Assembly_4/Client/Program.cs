b717244685fdbb9d94a0f8127290b3af0fd5cf38
johniepreston <nameispreston@gmail.com>
2017-04-15 10:56:30 +0500

    Add files via upload

---
+++ Assembly_4/Client/Program.cs
@@ -0,0 +1,23 @@
+﻿using ConcretePrototype;
+using Prototype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Text a = new Text("aaaaaa");
+            IPrototype b = a.Clone();
+            Text c;
+            if ((b as Text) != null)
+                c = (Text)b;
+        }
+
+    }
+}
---
+++ Assembly_4/ConcretePrototype/Text.cs
@@ -0,0 +1,34 @@
+﻿using Prototype;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConcretePrototype
+{
+    public class Text : IPrototype
+    {
+        public String Value
+        {
+            get
+            {
+                return _value;
+            }
+        }
+
+        public Text(String value)
+        {
+            _value = value.Clone().ToString();
+
+        }
+
+        public IPrototype Clone()
+        {
+            return new Text(Value);
+        }
+
+        private readonly String _value;
+
+    }
+}
