04a2c0206e6b737b642bd590b49028d513197d21
johniepreston <nameispreston@gmail.com>
2017-04-15 10:57:20 +0500

    Add files via upload

---
+++ Assembly_5/Main/Heart.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    public class Heart
+    {
+        public static Heart Instance()
+        {
+            return GetSingletonData();
+        }
+
+        protected static Heart GetSingletonData()
+        {
+            if (_instance == null)
+            {
+                _instance = new Heart();
+            }
+
+            return _instance;
+        }
+
+        protected Heart()
+        {
+        }
+
+        public double Frequence { get; set; }
+
+        private static Heart _instance;
+    }
+
+}
---
+++ Assembly_5/Main/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Heart heart = Heart.Instance();
+            heart.Frequence = 120;
+
+            Heart heart2 = Heart.Instance();
+
+            if (heart.Frequence == heart2.Frequence)
+                Console.WriteLine(heart2.Frequence + "=)");
+            else
+                Console.WriteLine(heart2.Frequence + "=(");
+
+            Console.Read();
+
+        }
+    }
+}
